dd7efabc2e8ab51b4ebef3548622189cef104462
Chinmay Kousik <kousikchinmay2@gmail.com>
2015-12-15 20:35:54 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var express = require('express'),
 app = express(),
-bodyParser = require('body-parser');
+bodyParser = require('body-parser'),
+http = require(http).createServer(app);
 
 app.use(bodyParser.json());
+
+app.post('/',function(req,res){
+	res.json({"home":"home"});
+});
+
+app.post('/login',function(req,res){
+	res.json({
+		"message":"login request from"+req.body.user
+	});
+});
+
+app.post('/feed',function(req,res){
+	res.json({
+		"message":"feed for "+req.body.feed
+	});
+});
